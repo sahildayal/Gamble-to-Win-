8cae6e020f522c2420917cd472368151a93e084c
Sahil Dayal <sd8804@rit.edu>
2023-07-12 11:36:15 -0400

    Added number of bets function

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
-        amount = input("What would you liek to deposit? $")
+        amount = input("What would you like to deposit? $")
         if amount.isdigit(): #we use isdigit because if they put negative it wont regit it so it takes care of that as well
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print("AMount must be greater than 0.")
+                print("Amount must be greater than 0.")
         else:
             print("Please enter a number!")
 
     return amount
 
+def get_numbers_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit: 
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid no of lines! ")
+        else:
+            print("Please enter a number!")
+
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_numbers_of_lines()
+    print(balance, ",", lines)
+
+
+main()
