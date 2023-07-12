c41a483d757777c306ed1d5467ee016b7f897fb4
Sahil Dayal <sd8804@rit.edu>
2023-07-12 12:29:54 -0400

    printed the bets in row order

--- main.py
+++ main.py
@@ -15,14 +15,25 @@ symbol_count={
     "D" : 8
 }
 
+symbol_value={
+    "A":  5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    
+
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items(): #.items will give me both the key and its value
         for _ in range(symbol_count):
             all_symbols.append(symbol) 
 
-    columns = [[], [], []]
-    for col in range(cols):
+    columns = []
+    for _ in range(cols):  
         column = []
         current_symbols = all_symbols[:]#slicing it because we need a copy
         for _ in range(rows):
@@ -32,7 +43,17 @@ def get_slot_machine_spin(rows, cols, symbols):
 
         columns.append(column)
 
+    return columns
 
+def print_slots_machine(columns):
+    #transposing
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i!= len(columns) - 1 : 
+                print(column[row], end=" | ")#tells what to end the line with
+            else:
+                print(column[row])
+        print()
 
 
 
@@ -93,5 +114,9 @@ def main():
             break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    print()
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slots_machine(slots)
 
 main()
